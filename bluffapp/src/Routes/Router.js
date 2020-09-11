ff23fc8466ecb7153241e21afdbf382c80f67bb6
Prashant <prashant.chaturvedi@joshtechnologygroup.com>
2020-09-11 21:14:36 +0530

    Created UI to create a game

--- bluffapp/src/Routes/Router.js
+++ bluffapp/src/Routes/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ROUTES from "../Constants/pathConstants";
 import LoginPage from "../Pages/LoginPage";
 import SignUpPage from "../Pages/SignUpPage";
+import HomePage from "../Pages/HomePage";
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ const AppRouter = () => (
       <Switch>
         <Route path={ROUTES.LOGIN_ROUTE} component={LoginPage} exact={true} />
         <Route path={ROUTES.SIGNUP_ROUTE} component={SignUpPage} exact={true} />
+        <Route path={ROUTES.HOME_ROUTE} component={HomePage} exact={true} />
       </Switch>
     </div>
   </BrowserRouter>
