04ee2ff8abb22c64fbe606fed8ef87b729fba3db
Prashant <prashant.chaturvedi@joshtechnologygroup.com>
2020-09-15 18:15:17 +0530

    Calls Api CreateGame from UI

--- bluffapp/src/Constants/urlConstants.js
+++ bluffapp/src/Constants/urlConstants.js
@@ -3,6 +3,7 @@ const BASE_URL = "http://127.0.0.1:8000";
 const API_URL = {
   validateUrl: `${BASE_URL}/accounts/login/`,
   createUrl: `${BASE_URL}/accounts/users/`,
+  createGameUrl: `${BASE_URL}/game/create/`,
 };
 
 export default API_URL;
