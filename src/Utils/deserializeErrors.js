4e4c40d0bc94aa42050cdee8364e024fd213af65
Naveen Rathee <naveen.kumar1@joshtechnologygroup.com>
2020-09-18 14:12:08 +0530

    API error handling

---
+++ src/Utils/deserializeErrors.js
@@ -0,0 +1,27 @@
+/**
+ * returns a string describing staus code category
+ * @param {*} status 
+ */
+export default function deserializeErrors(status){
+    statusCategory = Math.floor(status/100)
+    switch(statusCategory){
+        case 1:
+            return 'Continue'
+            
+        case 2:
+            return 'Success'
+            
+        case 3:
+            return 'Redirect'
+            
+        case 4:
+            return 'Client Error'
+            
+        case 5:
+            return 'Internal Server Error'
+            
+        default:
+            return ''
+            
+    }
+}
