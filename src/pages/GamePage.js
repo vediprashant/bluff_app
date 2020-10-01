158b122679da70a2f2729855f0396b31a2e919cb
Prashant <prashant.chaturvedi@joshtechnologygroup.com>
2020-10-01 11:22:06 +0530

    adds button to start game

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import SelectSet from "../Components/SelectSet";
 import Button from "../Components/Button";
 import PlayerCards from "../Components/PlayerCards";
-import { Route } from "react-router-dom";
 import connectToGame from "../actionCreators/connectToGame";
 import TableCards from "../Components/TableCards";
 import Players from "../Components/Players";
@@ -30,25 +29,36 @@ class GamePage extends Component {
       mappedCards.push(cardsSelected[card].id);
     }
   };
+  startGame = () => {
+    //Action to start the game
+  };
 
   render() {
     return (
-      <div>
-        <div className="gameScreen">
-          <div className="timer"></div>
-          <Players
-            game_players={this.props.game.gameState.game_players}
-            self={this.props.game.gameState.self}
-          />
-          <div className="tableCards">
-            <TableCards card_count={5} />
-            {/*GAME TABLE CARD COUNT GOES HERE */}
-          </div>
+      <div className="gameScreen">
+        <div className="timer"></div>
+        <Players
+          game_players={this.props.game.gameState.game_players}
+          self={this.props.game.gameState.self}
+        />
+        <div className="tableCards">
+          <TableCards card_count={5} />
+          {/*GAME TABLE CARD COUNT GOES HERE */}
         </div>
         <div className="actionButtons">
           <Button text={"Pass"} color={"purple"} />
           <Button text={"Play"} color={"purple"} onClick={this.playCards} />
           <Button text={"Show"} color={"purple"} />
+          {/* {console.log(this.props.game)} */}
+          {this.props?.game?.gameState?.game?.owner ===
+            this.props?.game?.gameState?.self?.user?.id &&
+          !this.props?.gameState?.game?.started ? (
+            <Button
+              text={"Start Game"}
+              color={"grey"}
+              className={"startGame"}
+            />
+          ) : null}
         </div>
         <div className="playerCard">
           <h1 className="heading1">Select Cards</h1>
