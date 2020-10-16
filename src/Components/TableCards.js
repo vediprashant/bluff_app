5b772509d9fb6b72a38d79f5b4f257718e26fb66
Prashant <prashant.chaturvedi@joshtechnologygroup.com>
2020-10-16 17:13:35 +0530

    updated random table cards

--- src/Components/TableCards.js
+++ src/Components/TableCards.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 /**
  * Takes table card count, renders those cards
  * @param {*} props
  */
 export default function TableCards(props) {
-  let card = 0;
-  let temp = [];
-  const card_count = props.card_count;
-  const colors = ["blue", "gray", "green", "purple", "red", "yellow"];
-  while (card < card_count) {
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    let deg = Math.floor(Math.random() * 100 + 1);
-    temp.push(
-      <img
-        key={temp.length}
-        className="cards"
-        src={`../PNG/${color}_back.png`}
-        style={{ transform: `rotate(${deg}deg)` }}
-        alt="card"
-      ></img>
-    );
-    card++;
-  }
-  return <div>{temp}</div>;
+  const [cards, setcards] = useState([]);
+  useEffect(() => {
+    let temp = [];
+    let card = 0;
+    const card_count = props.card_count;
+    const colors = ["blue", "gray", "green", "purple", "red", "yellow"];
+    while (card < card_count) {
+      let color = colors[Math.floor(Math.random() * colors.length)];
+      let deg = Math.floor(Math.random() * 100 + 1);
+      temp.push(
+        <img
+          key={temp.length}
+          className="cards"
+          src={`../PNG/${color}_back.png`}
+          style={{ transform: `rotate(${deg}deg)` }}
+          alt="card"
+        ></img>
+      );
+      card++;
+    }
+    setcards(temp);
+  }, [props.card_count]);
+
+  return <div>{cards}</div>;
 }
